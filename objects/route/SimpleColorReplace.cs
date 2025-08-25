d2f5a492e068946ab680d29e412df0a50cc9fb38
zrll <46812903+zrll12@users.noreply.github.com>
2025-08-25 11:41:31 +0800

    lint

--- objects/route/SimpleColorReplace.cs
+++ objects/route/SimpleColorReplace.cs
@@ -3,18 +3,18 @@ using Godot;
 namespace RensaSimulator.objects.route;
 
 public partial class SimpleColorReplace : Node2D {
-    [Export]
-    public Color RouteColor {
-        get => _spriteMaterial.GetShaderParameter("to").AsColor();
-        set => _spriteMaterial.SetShaderParameter("to", value);
-    }
+	[Export]
+	public Color RouteColor {
+		get => _spriteMaterial.GetShaderParameter("to").AsColor();
+		set => _spriteMaterial.SetShaderParameter("to", value);
+	}
 
-    private ShaderMaterial _spriteMaterial = new();
+	private ShaderMaterial _spriteMaterial = new();
 
-    public override void _Ready() {
-        _spriteMaterial.Shader = GD.Load<Shader>("res://shader/color_replace.gdshader");
-        _spriteMaterial.SetShaderParameter("from", new Color("#1c499f"));
+	public override void _Ready() {
+		_spriteMaterial.Shader = GD.Load<Shader>("res://shader/color_replace.gdshader");
+		_spriteMaterial.SetShaderParameter("from", new Color("#1c499f"));
 
-        GetNode<Sprite2D>("Sprite2D").Material = _spriteMaterial;
-    }
+		GetNode<Sprite2D>("Sprite2D").Material = _spriteMaterial;
+	}
 }
--- scene/GameScene.cs
+++ scene/GameScene.cs
@@ -7,114 +7,114 @@ using RensaSimulator.objects;
 namespace RensaSimulator.scene;
 
 public partial class GameScene : Node2D {
-    public override void _Ready() {
-        GenerateMap();
-    }
+	public override void _Ready() {
+		GenerateMap();
+	}
 
-    public override void _Process(double delta) { }
+	public override void _Process(double delta) { }
 
-    public override void _PhysicsProcess(double delta) {
-        GameManager.Tick(delta);
-    }
+	public override void _PhysicsProcess(double delta) {
+		GameManager.Tick(delta);
+	}
 
-    // Map
-    private void GenerateMap() {
-        GameManager.CurrentMap.ApplyTo(this);
-    }
+	// Map
+	private void GenerateMap() {
+		GameManager.CurrentMap.ApplyTo(this);
+	}
 
 
-    // Inputs
-    private bool _isMouseDown;
-    private Vector2 _startPos = Vector2.Zero;
-    private int _hoveredValue;
-    private Switch _hoveredItem;
-    private bool _endHoverWhilePressing;
-    private Switch _preHoveredItem;
+	// Inputs
+	private bool _isMouseDown;
+	private Vector2 _startPos = Vector2.Zero;
+	private int _hoveredValue;
+	private Switch _hoveredItem;
+	private bool _endHoverWhilePressing;
+	private Switch _preHoveredItem;
 
-    public override void _Input(InputEvent @event) {
-        switch (@event) {
-            case InputEventMouseButton mouseButton:
-                _isMouseDown = mouseButton.Pressed;
-                _startPos = mouseButton.Position;
+	public override void _Input(InputEvent @event) {
+		switch (@event) {
+			case InputEventMouseButton mouseButton:
+				_isMouseDown = mouseButton.Pressed;
+				_startPos = mouseButton.Position;
 
-                if (mouseButton.IsPressed()) {
-                    _hoveredValue = Int32.MaxValue;
-                    break;
-                }
+				if (mouseButton.IsPressed()) {
+					_hoveredValue = Int32.MaxValue;
+					break;
+				}
 
-                if (_endHoverWhilePressing && _hoveredItem != null) {
-                    _hoveredItem.HoverEnd();
-                    _hoveredItem = null;
-                    _endHoverWhilePressing = false;
-                }
+				if (_endHoverWhilePressing && _hoveredItem != null) {
+					_hoveredItem.HoverEnd();
+					_hoveredItem = null;
+					_endHoverWhilePressing = false;
+				}
 
-                if (_preHoveredItem != null) {
-                    _preHoveredItem.HoverStart();
-                    _hoveredItem = _preHoveredItem;
-                    _preHoveredItem = null;
-                }
+				if (_preHoveredItem != null) {
+					_preHoveredItem.HoverStart();
+					_hoveredItem = _preHoveredItem;
+					_preHoveredItem = null;
+				}
 
-                break;
-            case InputEventMouseMotion mouseMotion:
-                GetNode<Node2D>("MousePosition").Position = mouseMotion.Position - GetViewportRect().Size / 2;
-                OnMouseMotion(mouseMotion.Position);
-                break;
-            case InputEventScreenTouch touch:
-                GodotLogger.LogInfo($"Touch Button Pressed: {touch.Pressed}");
-                break;
-        }
-    }
+				break;
+			case InputEventMouseMotion mouseMotion:
+				GetNode<Node2D>("MousePosition").Position = mouseMotion.Position - GetViewportRect().Size / 2;
+				OnMouseMotion(mouseMotion.Position);
+				break;
+			case InputEventScreenTouch touch:
+				GodotLogger.LogInfo($"Touch Button Pressed: {touch.Pressed}");
+				break;
+		}
+	}
 
-    private void OnMouseAreaEntered(Area2D area) {
-        if (area is not Node2D node || node.GetParent() is not Switch parent) return;
+	private void OnMouseAreaEntered(Area2D area) {
+		if (area is not Node2D node || node.GetParent() is not Switch parent) return;
 
-        if (_isMouseDown) {
-            _preHoveredItem = parent;
-            return;
-        }
+		if (_isMouseDown) {
+			_preHoveredItem = parent;
+			return;
+		}
 
-        if (_hoveredItem != null && _hoveredItem.Id != parent.Id) {
-            _hoveredItem.HoverEnd();
-        }
+		if (_hoveredItem != null && _hoveredItem.Id != parent.Id) {
+			_hoveredItem.HoverEnd();
+		}
 
-        parent.HoverStart();
-        _hoveredItem = parent;
-    }
+		parent.HoverStart();
+		_hoveredItem = parent;
+	}
 
-    private void OnMouseAreaExited(Area2D area) {
-        if (area is not Node2D node || node.GetParent() is not Switch parent) return;
+	private void OnMouseAreaExited(Area2D area) {
+		if (area is not Node2D node || node.GetParent() is not Switch parent) return;
 
-        if (_isMouseDown) {
-            if (_hoveredItem.Id == parent.Id) {
-                _endHoverWhilePressing = true;
-                return;
-            }
+		if (_isMouseDown) {
+			if (_hoveredItem.Id == parent.Id) {
+				_endHoverWhilePressing = true;
+				return;
+			}
 
-            if (_preHoveredItem.Id == parent.Id) {
-                _preHoveredItem = null;
-                return;
-            }
-        }
+			if (_preHoveredItem.Id == parent.Id) {
+				_preHoveredItem = null;
+				return;
+			}
+		}
 
-        if (_hoveredItem.Id == parent.Id) {
-            parent.HoverEnd();
-            _hoveredItem = null;
-        }
-    }
+		if (_hoveredItem.Id == parent.Id) {
+			parent.HoverEnd();
+			_hoveredItem = null;
+		}
+	}
 
-    private void OnMouseMotion(Vector2 mousePosition) {
-        if (!_isMouseDown || _hoveredItem == null) return;
+	private void OnMouseMotion(Vector2 mousePosition) {
+		if (!_isMouseDown || _hoveredItem == null) return;
 
-        var delta = mousePosition.X - _startPos.X;
-        switch (delta) {
-            case > 60:
-                _startPos = mousePosition;
-                _hoveredValue = _hoveredItem.TurnRight();
-                break;
-            case < -60:
-                _startPos = mousePosition;
-                _hoveredValue = _hoveredItem.TurnLeft();
-                break;
-        }
-    }
+		var delta = mousePosition.X - _startPos.X;
+		switch (delta) {
+			case > 60:
+				_startPos = mousePosition;
+				_hoveredValue = _hoveredItem.TurnRight();
+				break;
+			case < -60:
+				_startPos = mousePosition;
+				_hoveredValue = _hoveredItem.TurnLeft();
+				break;
+		}
+	}
 }
